5a80f38496d91d7b1eaada0a8f6fadeb958464d3
ZachValencik <zvalencik01@aurora.edu>
2019-11-28 10:40:38 -0600

    In AdminEditInv, added binary search, bubble sort, TreeSet, ArrayList

--- src/finalProject/AdminEditInvController.java
+++ src/finalProject/AdminEditInvController.java
@@ -6,7 +6,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 import javafx.event.ActionEvent;
@@ -27,6 +30,8 @@ public class AdminEditInvController {
 	private float price;
 	private String company;
 	private boolean exist = false;
+	ArrayList<Integer> search = new ArrayList<Integer>();
+	private TreeMap<String, ProductsObj> itemTMap = new TreeMap<String, ProductsObj>();
 	@FXML
 	Text editingTxt;
 	@FXML
@@ -106,30 +111,107 @@ public class AdminEditInvController {
 		ResultSet r = null;
 
 		r = stmt.executeQuery("SELECT * FROM `" + part + "` limit 10000");
-
+		int i = 0;
+		search = new ArrayList<Integer>();
 		while (r.next()) {
-			if (r.getInt(1) == Integer.parseInt(sIdField.getText())) {
-				exist = true;
-				idNum = r.getInt(1);
-				amt = r.getInt(2);
-				item = r.getString(3);
-				price = r.getFloat(4);
-				company = r.getString(5);
+			// search[i] = r.getInt(1);
+			search.add(r.getInt(1));
+			itemTMap.put(
+					Integer.toString(r.getInt(1)),
+					new ProductsObj(Integer.toString(r.getInt(1)), Integer
+							.toString(r.getInt(2)), r.getString(3), Float
+							.toString(r.getFloat(4)), r.getString(5)));
+			i++;
 
-				sTxt.setText(r.getInt(1) + " " + r.getInt(2) + " "
-						+ r.getString(3) + " " + r.getFloat(4) + " "
-						+ r.getString(5));
-				break;
-			}
-			if (!exist) {
-				sTxt.setText("ID Not Found");
-			}
+			/*
+			 * if (r.getInt(1) == Integer.parseInt(sIdField.getText())) { exist
+			 * = true; idNum = r.getInt(1); amt = r.getInt(2); item =
+			 * r.getString(3); price = r.getFloat(4); company = r.getString(5);
+			 * 
+			 * sTxt.setText(r.getInt(1) + " " + r.getInt(2) + " " +
+			 * r.getString(3) + " " + r.getFloat(4) + " " + r.getString(5));
+			 * break; }
+			 */
 
 		}
+		search = sortBubble(search);
 
+		int a = 0;
+		int l = 0;
+		int[] array = new int[search.size()];
+
+		for (int b = 0; b < search.size(); b++) {
+
+			array[b] = search.get(b);
+		}
+		System.out.println(Arrays.toString(array));
+		int num = binarySearch(array, l, array.length - 1,
+				Integer.parseInt(sIdField.getText()));
+
+		if (num == -1) {
+
+			sTxt.setText("ID Not Found");
+		} else {
+			num++;
+			String changeIN = itemTMap.get(Integer.toString(num)).getItemNum();
+			String changeS = itemTMap.get(Integer.toString(num)).getStock();
+			String changeP = itemTMap.get(Integer.toString(num)).getPrice();
+			idNum = Integer.parseInt(changeIN);
+			amt = Integer.parseInt(changeS);
+			item = itemTMap.get(Integer.toString(num)).getItem();
+			price = Float.parseFloat(changeP);
+			company = itemTMap.get(Integer.toString(num)).getCompany();
+
+			sTxt.setText(itemTMap.get(Integer.toString(num)).getItemNum() + " "
+					+ itemTMap.get(Integer.toString(num)).getStock() + " "
+					+ itemTMap.get(Integer.toString(num)).getItem() + " "
+					+ itemTMap.get(Integer.toString(num)).getPrice() + " "
+					+ itemTMap.get(Integer.toString(num)).getCompany());
+			System.out.println("FOUND IT");
+			exist = true;
+		}
+	}
+
+	private int binarySearch(int[] arr, int l, int r, int x) {
+
+		if (r >= l) {
+			int mid = l + (r - l) / 2;
+
+			// If the element is present at the
+			// middle itself
+			if (arr[mid] == x)
+				return mid;
+
+			// If element is smaller than mid, then
+			// it can only be present in left subarray
+			if (arr[mid] > x)
+				return binarySearch(arr, l, mid - 1, x);
+
+			// Else the element can only be present
+			// in right subarray
+			return binarySearch(arr, mid + 1, r, x);
+		}
+
+		// We reach here when element is not present
+		// in array
+		return -1;
+	}
+
+	private ArrayList<Integer> sortBubble(ArrayList<Integer> search2) {
+		int n = search2.size();
+		for (int i = 0; i < n - 1; i++)
+			for (int j = 0; j < n - i - 1; j++)
+				if (search2.get(j) > search2.get(j + 1)) {
+
+					int temp = search2.get(j);
+					search2.add(j, search2.get(j + 1));
+					search2.add(j + 1, temp);
+				}
+		return search2;
 	}
 
 	public void addAmt(ActionEvent event) throws SQLException {
+
 		if (exist) {
 			String DBPath = "45.55.136.114/computerParts";
 			String fName = "pw.txt";
@@ -169,7 +251,8 @@ public class AdminEditInvController {
 						+ idNum;
 
 			} else if (part.equals("POWER")) {
-				q = "UPDATE `POWER` SET `POW_NUM`='" + idNum + "',`POW_AMOUNT`='"
+				q = "UPDATE `POWER` SET `POW_NUM`='" + idNum
+						+ "',`POW_AMOUNT`='"
 						+ (amt + Integer.parseInt(addAmtField.getText()))
 						+ "',`POW_NAME`='" + item + "',`POW_PRICE`='" + price
 						+ "',`POW_COMPANY`='" + company + "' WHERE `POW_NUM` ="
